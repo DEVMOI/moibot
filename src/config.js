8f3de8de18ad7fae7c8f211cf778e518f34a50c4
Warren Gates <warrenckhan@gmail.com>
2019-10-21 15:57:07 -0400

    created a basic chat filter that deletes a users message if it contains a specific phrase or word

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 import Eris from 'eris';
-const config = new Eris.CommandClient(
-	process.env.BOT_TOKEN,
-	{},
-	{
-		description: 'A Bot For the People',
-		owner: '@Moikune || https://moisite.herokuapp.com',
-		prefix: process.env.prefix
-	}
-);
+const config = {
+	moiBot: new Eris.CommandClient(
+		process.env.BOT_TOKEN,
+		{},
+		{
+			description: 'A Bot For the People',
+			owner: '@Moikune || https://moisite.herokuapp.com',
+			prefix: process.env.prefix
+		}
+	),
+	bannedWords: ['nigger']
+};
 export default config;
